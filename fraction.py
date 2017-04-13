1ebcd7a0f28f52d224d36c788b4b444c7add069e
IvanMelnychenko <i.melnichenko@skywell.com.ua>
2017-04-13 16:32:06 +0300

    fraction realize operator '-'

--- fraction.py
+++ fraction.py
@@ -1,14 +1,35 @@
 class Fraction:
+    # Constructor
     def __init__(self, top, bottom):
         self.num = top
         self.den = bottom
 
+    # toString
     def __str__(self):
+        if self.den < 0:
+            self.num = -self.num
+            self.den = -self.den
+
         return str(self.num) + '/' + str(self.den)
 
-    def __add__(self, other_fraction):
-        newnum = self.num * other_fraction.den + other_fraction.num * self.den
-        newden = self.den * other_fraction.den
+    # overload p1 + p2
+    def __add__(self, other):
+        newnum = self.num * other.den + other.num * self.den
+        newden = self.den * other.den
+        common = gcd(newnum, newden)
+
+        return Fraction(newnum//common, newden//common)
+
+    # overload equal
+    def __eq__(self, other):
+        firstnum = self.num * other.den
+        secondnum = other.num * self.den
+
+        return firstnum == secondnum
+
+    def __sub__(self, other):
+        newnum = self.num * other.den - other.num * self.den
+        newden = self.den * other.den
         common = gcd(newnum, newden)
 
         return Fraction(newnum//common, newden//common)
@@ -16,7 +37,6 @@ class Fraction:
 
 def gcd(m, n):
     while m != 0 and n != 0:
-        print(m == 0, n == 0)
         if m > n:
             m = m % n
         else:
--- starter.py
+++ starter.py
@@ -1,6 +1,8 @@
 from fraction import Fraction
 
 f1 = Fraction(2, 4)
-f2 = Fraction(2, 4)
+f2 = Fraction(3, 4)
 
 print(f1 + f2)
+print(f1 == f2)
+print(f1 - f2)
