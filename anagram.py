3a4594e9f9c2f3f8b9d06c63e9244518d6bc5620
IvanMelnychenko <i.melnichenko@skywell.com.ua>
2017-04-20 18:07:02 +0300

    anagram solution 2

--- anagram.py
+++ anagram.py
@@ -1,15 +1,13 @@
-def anagram_solution(s1, s2):
+def anagram_solution1(s1, s2):
     alist = list(s2)
 
-    pos1 = 0
     stillOk = True
+    pos1 = 0
 
     while pos1 < len(s1) and stillOk:
         pos2 = 0
         found = False
-
         while pos2 < len(alist) and not found:
-            print(s1[pos1], ' ', alist[pos2])
             if s1[pos1] == alist[pos2]:
                 found = True
             else:
@@ -21,7 +19,28 @@ def anagram_solution(s1, s2):
             stillOk = False
 
         pos1 = pos1 + 1
-    print(alist)
+
     return stillOk
 
-print(anagram_solution('python', 'typhon'))
+
+def anagram_solution2(s1, s2):
+    alist1 = list(s1)
+    alist2 = list(s2)
+
+    alist1.sort()
+    alist2.sort()
+
+    pos = 0
+    matches = True
+
+    while pos < len(s1) and matches:
+        if alist1[pos] == alist2[pos]:
+            pos = pos + 1
+        else:
+            matches = False
+
+    return matches
+
+
+print(anagram_solution1('python', 'thonpy'))
+print(anagram_solution2('pythonz', 'thonpy'))
