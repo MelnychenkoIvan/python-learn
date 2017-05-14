3f6bf7ea43b63fadcc87e0e02235037888473ca7
Ivan <melnychenko.ivan09@gmail.com>
2017-05-15 01:17:43 +0300

    password generation

--- programmin_python/test_2.py
+++ programmin_python/test_2.py
@@ -1,3 +1,21 @@
-arr = [1, 'a', 3, 4, 5, 6]
-a, *b = range(0, 4)
-print(a, b)
+import random
+
+
+def pass_generator(count_char=8):
+    arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g',
+           'h', 'i', 'j', 'k', 'l', 'm', 'n',
+           'o', 'p', 'q', 'r', 's', 't', 'u',
+           'v', 'w', 'x', 'y', 'z',
+           'A', 'B', 'C', 'D', 'E', 'F', 'G',
+           'H', 'I', 'J', 'K', 'L', 'M', 'N',
+           'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+           'V', 'W', 'X', 'Y', 'Z',
+           '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    passw = []
+
+    for i in range(count_char):
+        passw.append(random.choice(arr))
+    return "".join(passw)
+
+
+print(pass_generator(10))
