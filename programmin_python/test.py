562fe98250895eefd73698c4cb26441b191933b4
Ivan <melnychenko.ivan09@gmail.com>
2017-05-16 23:37:39 +0300

    string format example

--- programmin_python/test.py
+++ programmin_python/test.py
@@ -1,7 +1,18 @@
 """This is the command of our module"""
+file = open('index.html', 'r', encoding='utf-8')
+template_file = file.read()
+template_var = {"title": 'Some Title'}
+print(template_file.format(**template_var))
 
+file.close()
 
-def func():
-    """This is the description function"""
-    pass
-
+html = """
+<html>
+    <head>
+        <title>{title}</title>
+    </head>
+    <body>
+        <h1>{main_title}</h1>
+    </body>
+</html>
+""".format(**{"title": "title", "main_title": "Main Title"})
