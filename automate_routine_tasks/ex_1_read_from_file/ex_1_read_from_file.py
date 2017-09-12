aeaf98f2c63acab4f0226f776e6b12e6cb500b16
ivan <i.melnichenko@skywell.com.ua>
2017-09-12 17:05:14 +0300

    [remove] old examples

--- automate_routine_tasks/ex_1_read_from_file/ex_1_read_from_file.py
+++
@@ -1,9 +0,0 @@
-password_file = open('SecretPasswordFile.txt')
-secret_password = password_file.read()
-print('Enter your password:')
-typed_password = input()
-
-if typed_password == secret_password:
-    print('Access granted!')
-else:
-    print('Access denied!!!')
--- programming_on_python/ex_1_humen_database/dump_db_file.py
+++
@@ -1,6 +0,0 @@
-from programming_on_python.ex_1_humen_database.make_db_file import load_db
-
-db = load_db()
-
-for kye in db:
-    print(kye, '=>', db[kye])
