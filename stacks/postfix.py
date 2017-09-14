e6500de8f22626a379ba32fa98a799d0426c0297
ivan <i.melnichenko@skywell.com.ua>
2017-09-14 15:08:15 +0300

    [postfix] count postfix expression

--- stacks/postfix.py
+++ stacks/postfix.py
@@ -3,6 +3,7 @@ from stacks.stack import Stack
 
 def infix_to_postfix(infix_expr):
     alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    infix_list = infix_expr.split()
     postfix_list = []
     oper = dict()
     oper["*"] = 3
@@ -12,7 +13,7 @@ def infix_to_postfix(infix_expr):
     oper["("] = 1
     op_stack = Stack()
 
-    for token in infix_expr:
+    for token in infix_list:
         if token in alphabet:
             postfix_list.append(token)
         elif token == "(":
@@ -33,6 +34,34 @@ def infix_to_postfix(infix_expr):
     return " ".join(postfix_list)
 
 
-print(infix_to_postfix("(A+ B)*(C+D)"))
+def count_postfix(postfix_expr):
+    alphabet = '0123456789'
+    op_stack = Stack()
+
+    for token in postfix_expr:
+        if token in alphabet:
+            op_stack.push(int(token))
+        elif token in "*/+-":
+            op1 = op_stack.pop()
+            op2 = op_stack.pop()
+            res = do_math(token, op1, op2)
+            op_stack.push(res)
+
+    return op_stack.pop()
+
+
+def do_math(op, op1, op2):
+    if op == "*":
+        return op1 * op2
+    elif op == "/":
+        return op1 / op2
+    elif op == "+":
+        return op1 + op2
+    elif op == "-":
+        return op1 - op2
+
+
 print(infix_to_postfix("( A + B ) * C"))
 print(infix_to_postfix("A + B * C"))
+print(infix_to_postfix("( 6 + 3 ) * ( 2 + 1 )"))
+print(count_postfix(infix_to_postfix("( 1 + 1 ) * ( 1 + 1 )")))
